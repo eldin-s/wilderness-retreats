def159eac707b3c80d300d8f8c265fa11d3bd144
eldin-s <eldin_s12@hotmail.com>
2024-03-03 13:11:14 +0100

    Sorting system for cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -11,6 +11,7 @@ const CabinTable = () => {
 
   if (isLoading) return <Spinner />;
 
+  // 1. FILTER
   const filterValue = searchParams.get("discount") || "all";
 
   let filteredCabins;
@@ -19,6 +20,15 @@ const CabinTable = () => {
     filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
   if (filterValue === "with-discount")
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+  // 2. SORT
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  console.log(field, direction);
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
 
   return (
     <Menus>
@@ -34,7 +44,8 @@ const CabinTable = () => {
 
         <Table.Body
           // data={cabins}
-          data={filteredCabins}
+          // data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
